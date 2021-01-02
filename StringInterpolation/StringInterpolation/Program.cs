2bd64917a2d9349b7b0146ae3a2ffcd172d62953
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-01-02 15:21:01 +0100

    String Interpolation

---
+++ StringInterpolation/StringInterpolation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // new way to construct strings with placeholders or by using the plus sign to concatenate them
+
+        string shirtColor = "green";
+
+        //string someString = "I have a " + shirtColor + " shirt";
+        //Console.WriteLine(someString);
+        //string someString = String.Format("I have a {0} shirt", shirtColor);
+        //Console.WriteLine(someString);
+
+        string someString = $"I have a {shirtColor} shrit";
+        Console.WriteLine(someString);
+
+        // Escaping
+        string programFolder = "Program";
+        string programName = "Program.exe";
+
+        //string text = $"C:\\{programFolder}\\{programName}";
+        string text = $@"C:\{programFolder}\{programName}";
+
+        Console.WriteLine(text);
+
+        // expressions and methods inside  an interpolated expression
+        int number = 5;
+
+        //string text2 = $"The {number} is {(number % 2 == 0 ? "even" : "old" )}";
+        string text2 = $"The {number} is {checkEven(number)}";
+
+        Console.WriteLine(text2);
+    }
+
+    static string checkEven(int num)
+    {
+        return (num % 2 == 0 ? "even" : "old");
+    }
+}
